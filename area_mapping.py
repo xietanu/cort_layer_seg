db0fd80b1264ef12a12d25979f204ae65287160e
xietanu <100538668+xietanu@users.noreply.github.com>
2024-03-26 08:06:40 +0100

    Add functions to add info to patches to allow conditioning

---
+++ area_mapping.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+import numpy as np
+
+import cort
+import cort.load
+
+
+DATA_FOLDER = "data/cort_patches"
+
+
+def main():
+    patch_paths = cort.load.find_all_patches(DATA_FOLDER)
+    patches = cort.load.load_patches(patch_paths, report_progress=True)
+
+    print(f"Loaded {len(patches)} patches.")
+
+    all_brain_areas = set(patch.brain_area for patch in patches)
+
+    print(f"Found {len(all_brain_areas)} brain areas.")
+
+    one_hot_mapping = {
+        brain_area: np.eye(len(all_brain_areas))[i].tolist()
+        for i, brain_area in enumerate(all_brain_areas)
+    }
+
+    with open(os.path.join(DATA_FOLDER, "area_mapping.json"), "w") as f:
+        json.dump(one_hot_mapping, f)
+
+    print("Saved area mapping to area_mapping.json.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ create_data_splits.py
@@ -0,0 +1,9 @@
+import datasets
+
+
+def main():
+    datasets.create_tvt_splits("data/cort_patches")
+
+
+if __name__ == "__main__":
+    main()
