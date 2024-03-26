89a3b763f1b622515a3a86df7d46ce2a75f2090d
xietanu <100538668+xietanu@users.noreply.github.com>
2024-03-26 08:07:49 +0100

    Add dice and tversky loss functions

---
+++ nnet/loss/__init__.py
@@ -0,0 +1,2 @@
+from nnet.loss.dice_loss import dice_loss
+from nnet.loss.tversky_loss import tversky_loss
---
+++ nnet/loss/tversky_loss.py
@@ -0,0 +1,34 @@
+import torch
+
+
+def tversky_loss(
+    inputs: torch.Tensor,
+    targets: torch.Tensor,
+    ignore_index: int,
+    epsilon: float = 1e-6,
+) -> torch.Tensor:
+    """Compute the Dice loss."""
+    n_classes = inputs.shape[1]
+
+    flat_inputs = inputs.permute(0, 2, 3, 1).reshape(-1, n_classes)
+    flat_inputs[targets.flatten() == ignore_index] = 0
+    flat_inputs = torch.nn.functional.softmax(flat_inputs, dim=1)
+
+    flat_targets = torch.nn.functional.one_hot(
+        targets.flatten().to(torch.int64), num_classes=n_classes
+    )
+
+    flat_targets[targets.flatten() == ignore_index] = 0
+
+    loss = -torch.sum(
+        torch.sum(flat_targets * flat_inputs, dim=0)
+        / (
+            torch.sum(flat_targets * flat_inputs, dim=0)
+            + 0.5 * torch.sum(flat_targets * (1 - flat_inputs), dim=0)
+            + 0.5 * torch.sum((1 - flat_targets) * flat_inputs, dim=0)
+            + epsilon
+        ),
+        dim=0,
+    ) / (n_classes - 1)
+
+    return loss
